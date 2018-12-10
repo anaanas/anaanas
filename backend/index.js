f295e2622b5f312f48d305f261690184684c6894
Mengqi Yu <mengqiy@google.com>
2018-12-09 23:51:50 -0800

    add createProduct and getProducts

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const mongodb = require('mongodb')
 const f = require('util').format;
 const MongoClient = mongodb.MongoClient;
+
+var client;
+var dbConnection;
 var db;
 
 const connectToDBIfNotAlive = async () =>  {
-	if (db && db.isConnected()) {
+	if (dbConnection && dbConnection.isConnected()) {
         return;
     }
 	const user = encodeURIComponent(process.env.MONGODB_USER);
@@ -12,17 +15,73 @@ const connectToDBIfNotAlive = async () =>  {
 	const host = process.env.MONGODB_HOST;
 	const authMechanism = 'DEFAULT';
 
+	// Database Name
+	const dbName = 'wanglai';
+
 	const uri = f('mongodb://%s:%s@%s:27017/?authMechanism=%s',
 	  user, password, host, authMechanism);
 
 	console.log("connecting : ", uri)
 
-    try {
-        db = await MongoClient.connect(uri, { useNewUrlParser: true });
+	try {
+		client = new MongoClient(uri)
+        dbConnection = await client.connect();
+        db = client.db(dbName);
     } catch (e) {
     	// TODO: custome error.
         throw e;
-    }
+	}
+}
+
+exports.createProduct = async (req, res) => {
+	if(req.method!='POST'){
+		console.log('only allow POST method at this endpoint');
+		res.status(405).send({ error: 'only allow POST method at this endpoint' });
+	}
+	if(req.get('content-type')!='application/json'){
+		console.log('only allow application/json at this endpoint');
+		res.status(400).send({ error: 'only allow application/json at this endpoint' });
+	}
+
+	await connectToDBIfNotAlive().catch(
+		(err) => {res.status(500).end(err.message);}
+	)
+
+	db.collection('products').insertOne(req.body, function(err, r) {
+		if(err){
+			throw err
+		}else{
+			insProdStr = JSON.stringify(req.body)
+			console.log(insProdStr +' has been inserted');
+			res.send(insProdStr +' has been inserted');
+		}
+	});
+}
+
+exports.getProducts = async (req, res) => {
+	if(req.method!='GET'){
+		console.log('only allow GET method at this endpoint');
+		res.status(405).send({ error: 'only allow GET method at this endpoint' });
+	}
+
+	await connectToDBIfNotAlive().catch(
+		(err) => {res.status(500).end(err.message);}
+	)
+	db.collection('products').find({}).toArray(function(err, docs) {		
+		if(err){
+			throw err
+		}else{
+			docStr = JSON.stringify(docs)
+			console.log('docs are ' + docStr);
+			res.send('docs are ' + docStr);
+		}
+	});
+}
+
+// Use this func to test if your local emulator works fine with ENV
+// Ref: https://github.com/GoogleCloudPlatform/cloud-functions-emulator/issues/178
+exports.getEnv = (req, res) => {
+	res.send('MONGODB_USER is ' + process.env.MONGODB_USER);
 }
 
 exports.getHelloWorld = (req, res) => {
@@ -34,6 +93,6 @@ exports.getHTTP = async (req, res) => {
 		(err) => {res.status(500).end(err.message);}
 	)
 
-	console.log('Hello World: db is ' + db.isConnected());
-	res.send('Hello World: db is ' + db.isConnected());
+	console.log('Hello World: db is ' + dbConnection.isConnected());
+	res.send('Hello World: db is ' + dbConnection.isConnected());
 }
