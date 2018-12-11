ea76622800077e4063b0884fd03ebcc356e8bb4d
ivdone <zivdone@gmail.com>
2018-12-10 22:32:09 -0800

    [code] Add login and authentication for each request.
    Sign and issue jwt token through cookie to support login.
    Add authentication for createProduct as example.

---
+++ backend/src/helper/cookieHelper.js
@@ -0,0 +1,9 @@
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie')
+
+
+exports.parseCookies = (req) => {
+    console.log(req.headers.cookie)
+    req.cookies = cookie.parse(req.headers.cookie, undefined)
+    req.cookies = cookieParser.JSONCookies(req.cookies)
+}
